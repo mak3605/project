cce16d218ef739f1bb15c2e8a686bd992b90118b
MAK <maswankumar3605@gmail.com>
2022-02-11 23:39:12 +0530

    commit on "soal"

---
+++ test2/File/README.md/index.js
@@ -0,0 +1,14 @@
+var carName = "Jaguar"
+
+
+<!DOCYTYPE html>
+<html>
+    <body>
+        <h2> My First Transcript</h2>
+        <button type="Button"
+        onclick="document.getElementById('demo').innerHTML = Date()">
+            Click me to display Date and Time.
+        </button>
+        <p id="demo"></p>
+    </body>
+</html>
